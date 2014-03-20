f63fb8044ce3c58b924091e2fc14be01aa03f400
godseraph <wangjch@sina.com>
2014-03-20 22:20:14 +0800

    在model中加入输入数据校验

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage="请输入你的名字")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "请输入你的邮件地址")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage="请输入有效的邮件地址")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage="请输入电话号码")]
         public string Phone { get; set; }
-        public bool? WillAttend { get; set; }       //? 代表可以为空
+        [Required(ErrorMessage="请设置是否参加")]
+        public bool? WillAttend { get; set; }       //? 代表可以为null
     }
 }
