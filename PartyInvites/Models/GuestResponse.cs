e4e07ebc8deffefa368d4ae9d966a90cef7cb624
godseraph <wangjch@sina.com>
2014-03-23 11:39:14 +0800

    使用Ace1.2UI模板进行界面美化

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -9,14 +9,18 @@ namespace PartyInvites.Models
     public class GuestResponse
     {
         [Required(ErrorMessage="请输入你的名字")]
+        [Display(Name="姓名")]
         public string Name { get; set; }
         [Required(ErrorMessage = "请输入你的邮件地址")]
         [RegularExpression(".+\\@.+\\..+",ErrorMessage="请输入有效的邮件地址")]
+        [Display(Name="邮箱")]
         public string Email { get; set; }
 
         [Required(ErrorMessage="请输入电话号码")]
+        [Display(Name="电话")]
         public string Phone { get; set; }
         [Required(ErrorMessage="请设置是否参加")]
+        [Display(Name="是否参加")]
         public bool? WillAttend { get; set; }       //? 代表可以为null
     }
 }
