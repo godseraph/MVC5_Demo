6e85bf29ab7b188ad8908041f04da87490d3163f
godseraph <wangjch@sina.com>
2014-03-23 23:44:06 +0800

    解决"根据验证过程，远程证书无效。"问题;将发送邮件的代码重构到控制器中。

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Helpers;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
 using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
@@ -27,7 +31,24 @@ namespace PartyInvites.Controllers
         {
             if (ModelState.IsValid)
             {
-                // TODO: Email response to the party organizer
+                try
+                {
+                    WebMail.SmtpServer = "smtp.sina.com";
+                    WebMail.SmtpPort = 587;
+                    WebMail.EnableSsl = true;
+                    WebMail.UserName = "godseraph_mvc";
+                    WebMail.Password = "mvc12345678";
+                    WebMail.From = "godseraph_mvc@sina.com";
+                    ServicePointManager.ServerCertificateValidationCallback = delegate(Object obj, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors) { return true; };
+                    WebMail.Send(guestResponse.Email, "RSVP Notification", guestResponse.Name + " is " + ((guestResponse.WillAttend ?? false) ? "" : "not") + "attending");
+                    ViewBag.SendState = true;
+                }
+                catch(Exception e)
+                {
+                    ViewBag.SendState = false;
+                    ViewBag.SendErrorMsg = e.Message;
+                }
+
                 return View("Thanks", guestResponse);
             }
             else
